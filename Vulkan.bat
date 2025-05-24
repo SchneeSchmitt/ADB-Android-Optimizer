2cc5d75e22dc6e3bee9530f89af308df1ebdbe54
Snoocomics <157796309+SchneeSchmitt@users.noreply.github.com>
2025-05-24 11:43:14 +0800

    Upload the core scripts

---
+++ Vulkan.bat
@@ -0,0 +1,5 @@
+adb shell setprop debug.hwui.renderer skiavk
+adb shell setprop debug.composition.type skiavk
+adb shell setprop debug.vulkan.layers.enable 1
+adb shell setprop debug.renderengine.backend skiavkthreaded
+adb shell setprop debug.rs.visual vulkan
