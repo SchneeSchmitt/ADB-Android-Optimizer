54957066b1f1675e22af9cdd016ff1d0d68247bb
Snoocomics <157796309+SchneeSchmitt@users.noreply.github.com>
2025-05-24 19:17:33 +0800

    Makes Hardware.bat caught on fire faster

--- Hardware.bat
+++ Hardware.bat
@@ -7,3 +7,5 @@ adb shell settings put global persist.sys.ui.hw 1
 adb shell settings put global debug.qc.hardware true
 adb shell settings put global video.accelerate.hw 1
 adb shell settings put global debug.qctwa.statusbar 1
+adb shell settings put global sys.hwc.gpu_perf_mode 1
+adb shell setprop debug.sf.enable_hwc_vds 1
