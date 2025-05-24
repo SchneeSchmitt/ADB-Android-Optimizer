045c687ac511a5d1cb046a88251095a77c5e70bc
Snoocomics <157796309+SchneeSchmitt@users.noreply.github.com>
2025-05-24 19:18:25 +0800

    More optimizations for Qualcom_only.bat

--- Qualcom_only.bat
+++ Qualcom_only.bat
@@ -5,3 +5,5 @@ adb shell setprop debug.mdpcomp.maxlayer 9
 adb shell setprop debug.mdpcomp.idletime -1
 adb shell settings put global persist.hwc.mdpcomp.enable true
 adb shell settings put global debug.qctwa.preservebuf 1
+adb shell settings put global vendor.gralloc.disable_ubwc 0
+adb shell settings put global sdm.debug.disable_skip_validate 1
