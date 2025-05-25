735d08f29ee8e00dffacb93d981667735db82663
Snoocomics <157796309+SchneeSchmitt@users.noreply.github.com>
2025-05-25 12:30:03 +0800

    burn Hardware.bat

--- Hardware.bat
+++ Hardware.bat
@@ -5,6 +5,7 @@ adb shell settings put global hw2d.force 1
 adb shell settings put global hw3d.force 1
 adb shell settings put global persist.sys.ui.hw 1
 adb shell settings put global debug.qc.hardware true
+adb shell settings put global com.qc.hardware true 
 adb shell settings put global video.accelerate.hw 1
 adb shell settings put global debug.qctwa.statusbar 1
 adb shell settings put global sys.hwc.gpu_perf_mode 1
