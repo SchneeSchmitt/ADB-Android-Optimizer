c49fa022ddf653d28b4590024aa5e2350e9439fd
Snoocomics <157796309+SchneeSchmitt@users.noreply.github.com>
2025-05-25 12:20:57 +0800

    Update Qualcom_only.bat

--- Qualcom_only.bat
+++ Qualcom_only.bat
@@ -5,5 +5,6 @@ adb shell setprop debug.mdpcomp.maxlayer 9
 adb shell setprop debug.mdpcomp.idletime -1
 adb shell settings put global persist.hwc.mdpcomp.enable true
 adb shell settings put global debug.qctwa.preservebuf 1
+adb shell setprop debug.qctwa.statusbar 1
 adb shell settings put global vendor.gralloc.disable_ubwc 0
 adb shell settings put global sdm.debug.disable_skip_validate 1
