c25073d59a4a60332d665abb51606d64d4a1862f
Snoocomics <157796309+SchneeSchmitt@users.noreply.github.com>
2025-06-10 06:02:55 +0800

    Qualcom_only.bat V2 update

--- Qualcom_only.bat
+++ Qualcom_only.bat
@@ -1,6 +1,7 @@
 adb shell settings put global composition.type mdp
 adb shell setprop debug.composition.type mdp
 adb shell settings put global persist.sys.composition.type mdp
+adb shell setprop debug.hwc.composition.type mdp
 adb shell setprop debug.mdpcomp.maxlayer 9
 adb shell setprop debug.mdpcomp.idletime -1
 adb shell settings put global persist.hwc.mdpcomp.enable true
