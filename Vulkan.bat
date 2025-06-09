02af331fb4d449d15da965e4dc7283fe7cc98c99
Snoocomics <157796309+SchneeSchmitt@users.noreply.github.com>
2025-06-10 05:30:05 +0800

    Vulkan.bat V2 update

--- Vulkan.bat
+++ Vulkan.bat
@@ -5,3 +5,4 @@ adb shell setprop debug.renderengine.backend skiavkthreaded
 adb shell setprop debug.hwui.renderer.backend skiavkthreaded
 adb shell setprop debug.rs.visual vulkan
 adb shell setprop debug.hwui.shadow.renderer skiavk
+adb shell setprop debug.vulkan.layers Skia,MoltenVK,VulkanRT
