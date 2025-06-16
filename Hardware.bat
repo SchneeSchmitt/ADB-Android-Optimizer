43bfbbd1d9e4716bcc6f991c6411668d6a6ae3e2
Snoocomics <157796309+SchneeSchmitt@users.noreply.github.com>
2025-06-16 19:27:14 +0800

    Hwc is more suitable in Hardware.bat

--- Hardware.bat
+++ Hardware.bat
@@ -27,3 +27,6 @@ adb shell setprop debug.sf.hwinterpolation 1
 adb shell setprop debug.gl.hw 1
 adb shell setprop debug.hwui.force_hw_layers 1
 adb shell setprop debug.hwui.use_hw_layers 1
+adb shell setprop debug.hwc.compose_level 1
+adb shell settings put global persist.hwc.ptor.enable true
+adb shell setprop debug.hwc.bq_count 3
